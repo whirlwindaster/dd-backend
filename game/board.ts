b39f6a662410c728ff3de9f4d3184c237fa9ce35
whirlwindaster <whirlwindaster@gmail.com>
2023-11-18 21:41:14 -0500

    committing this all now

---
+++ game/board.ts
@@ -0,0 +1,184 @@
+import {
+  Coordinate,
+  Direction,
+  Goal,
+  RobotColor,
+  RobotPositions,
+  Tile,
+} from "../lib/types.ts";
+import { setup } from "./boardsetter.ts";
+
+export default class Board {
+  size = 16;
+  tiles: Tile[][];
+  goals: Goal[];
+  current_positions: RobotPositions = {
+    r: { x: 0, y: 0 },
+    y: { x: 0, y: 0 },
+    g: { x: 0, y: 0 },
+    u: { x: 0, y: 0 },
+    b: { x: 0, y: 0 },
+  };
+  saved_positions: RobotPositions;
+
+  constructor(setup_num: number) {
+    ({ tiles: this.tiles, goals: this.goals } = setup[setup_num - 1]());
+    this.populateRobots();
+    this.saved_positions = { ...this.current_positions };
+  }
+
+  populateRobots() {
+    let key: keyof RobotPositions;
+    for (key in this.current_positions) {
+      let x = Math.floor(Math.random() * this.size),
+        y = Math.floor(Math.random() * this.size),
+        c: Coordinate = { x, y };
+
+      while (!this.isSpotAvailable(c)) {
+        x = Math.floor(Math.random() * this.size);
+        y = Math.floor(Math.random() * this.size);
+        c = { x, y };
+      }
+
+      this.current_positions[key] = { ...c };
+      this.tiles[c.x][c.y].robot = key; // this feels sus
+    }
+  }
+
+  saveRobotPositions() {
+    this.saved_positions = { ...this.current_positions };
+  }
+
+  resetRobotPositions() {
+    const old_robots = {...this.current_positions};
+
+    let key: keyof RobotPositions;
+    for (key in this.current_positions) {
+      this.tiles[this.current_positions[key].x][this.current_positions[key].y]
+        .robot = null;
+    }
+    this.current_positions = { ...this.saved_positions };
+    for (key in this.current_positions) {
+      this.tiles[this.current_positions[key].x][this.current_positions[key].y]
+        .robot = key;
+    }
+
+    return old_robots;
+  }
+
+  isSpotAvailable(c: Coordinate): boolean {
+    if (
+      this.tiles[c.x][c.y].robot || this.tiles[c.x][c.y].goal ||
+      this.isCenterSquare(c)
+    ) {
+      return false;
+    }
+
+    const left_bound: boolean = c.x === 0;
+    const up_bound: boolean = c.y === 0;
+
+    // literally no idea what this does LOL
+    return !(this.tiles[c.x][c.y].bottom_wall &&
+      this.tiles[c.x][c.y].right_wall &&
+      // tries to prevent from going out of bounds
+      !(!left_bound && !this.tiles[c.x - 1][c.y].right_wall) &&
+      !(!up_bound && !this.tiles[c.x][c.y - 1].bottom_wall));
+  }
+
+  isCenterSquare(c: Coordinate): boolean {
+    return (this.size / 2 - 1 <= c.x && c.x <= this.size / 2) &&
+      (this.size / 2 - 1 <= c.y && c.y <= this.size / 2);
+  }
+
+  shuffleGoals() {
+    let currentIndex = this.goals.length,
+      randomIndex: number;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [this.goals[currentIndex], this.goals[randomIndex]] = [
+        this.goals[randomIndex],
+        this.goals[currentIndex],
+      ];
+    }
+  }
+
+  getDestinationTile(direction: Direction, c: Coordinate): Tile {
+    switch (direction) {
+      case "up": {
+        for (let i = c.y - 1; i >= 0; i--) {
+          if (this.tiles[c.x][i].bottom_wall || this.tiles[c.x][i].robot) {
+            return this.tiles[c.x][i + 1];
+          }
+        }
+
+        return this.tiles[c.x][0];
+      }
+
+      case "left": {
+        for (let i = c.x - 1; i >= 0; i--) {
+          if (this.tiles[i][c.y].right_wall || this.tiles[i][c.y].robot) {
+            return this.tiles[i + 1][c.y];
+          }
+        }
+
+        return this.tiles[0][c.y];
+      }
+
+      case "down": {
+        for (let i = c.y + 1; i < this.size; i++) {
+          if (this.tiles[c.x][i].robot) {
+            return this.tiles[c.x][i - 1];
+          } else if (this.tiles[c.x][i].bottom_wall) {
+            return this.tiles[c.x][i];
+          }
+        }
+
+        return this.tiles[c.x][this.size - 1];
+      }
+
+      case "right": {
+        for (let i = c.x + 1; i < this.size; i++) {
+          if (this.tiles[i][c.y].robot) {
+            return this.tiles[i - 1][c.y];
+          } else if (this.tiles[i][c.y].right_wall) {
+            return this.tiles[i][c.y];
+          }
+        }
+
+        return this.tiles[this.size - 1][c.y];
+      }
+    }
+  }
+
+  moveRobot(robot: RobotColor, direction: Direction): Coordinate | null {
+    const destination_tile = this.getDestinationTile(direction, this.current_positions[robot]);
+    if (this.current_positions[robot].x === destination_tile.coord.x &&
+      this.current_positions[robot].y === destination_tile.coord.y) {
+        return null;
+      }
+    this.tiles[this.current_positions[robot].x][this.current_positions[robot].y].robot = null;
+    destination_tile.robot = robot;
+    this.current_positions[robot] = {...destination_tile.coord}
+    return {...destination_tile.coord};
+  }
+
+  isSolved(): boolean {
+    const robot_on_goal_tile = this
+      .tiles[this.goals[0].coord.x][this.goals[0].coord.y]
+      .robot;
+
+    if (robot_on_goal_tile) {
+      if (this.goals[0].color === "m") {
+        return true;
+      }
+      return (this.goals[0].color === robot_on_goal_tile);
+    }
+    return false;
+  }
+}
---
+++ game/boardsetter.ts
@@ -0,0 +1,118 @@
+import { BoardSetup, Coordinate, Goal, Tile } from "../lib/types.ts";
+
+const b1 = () => {
+  const rightWallCoord: Coordinate[] = [
+      { x: 3, y: 0 },
+      { x: 10, y: 0 },
+      { x: 8, y: 1 },
+      { x: 5, y: 2 },
+      { x: 14, y: 2 },
+      { x: 2, y: 4 },
+      { x: 10, y: 4 },
+      { x: 6, y: 5 },
+      { x: 0, y: 6 },
+      { x: 11, y: 6 },
+      { x: 3, y: 9 },
+      { x: 7, y: 10 },
+      { x: 12, y: 10 },
+      { x: 0, y: 11 },
+      { x: 10, y: 11 },
+      { x: 6, y: 12 },
+      { x: 13, y: 12 },
+      { x: 1, y: 14 },
+      { x: 9, y: 14 },
+      { x: 5, y: 15 },
+      { x: 11, y: 1 },
+    ],
+    bottomWallCoord: Coordinate[] = [
+      { x: 9, y: 1 },
+      { x: 14, y: 1 },
+      { x: 5, y: 2 },
+      { x: 2, y: 3 },
+      { x: 0, y: 4 },
+      { x: 10, y: 4 },
+      { x: 15, y: 4 },
+      { x: 1, y: 5 },
+      { x: 7, y: 5 },
+      { x: 12, y: 5 },
+      { x: 3, y: 8 },
+      { x: 15, y: 8 },
+      { x: 8, y: 9 },
+      { x: 13, y: 9 },
+      { x: 1, y: 11 },
+      { x: 10, y: 11 },
+      { x: 6, y: 12 },
+      { x: 14, y: 12 },
+      { x: 0, y: 13 },
+      { x: 2, y: 13 },
+      { x: 9, y: 13 },
+    ],
+    goals: Goal[] = [
+      { color: "g", shape: "star", coord: { x: 9, y: 1 } },
+      { color: "u", shape: "star", coord: { x: 5, y: 2 } },
+      { color: "y", shape: "star", coord: { x: 3, y: 9 } },
+      { color: "r", shape: "star", coord: { x: 13, y: 10 } },
+      { color: "g", shape: "crescent", coord: { x: 2, y: 4 } },
+      { color: "y", shape: "crescent", coord: { x: 14, y: 2 } },
+      { color: "r", shape: "crescent", coord: { x: 1, y: 11 } },
+      { color: "u", shape: "crescent", coord: { x: 9, y: 14 } },
+      { color: "g", shape: "planet", coord: { x: 10, y: 11 } },
+      { color: "r", shape: "planet", coord: { x: 10, y: 4 } },
+      { color: "y", shape: "planet", coord: { x: 1, y: 6 } },
+      { color: "u", shape: "planet", coord: { x: 6, y: 12 } },
+      { color: "g", shape: "gear", coord: { x: 2, y: 14 } },
+      { color: "r", shape: "gear", coord: { x: 7, y: 5 } },
+      { color: "u", shape: "gear", coord: { x: 12, y: 6 } },
+      { color: "y", shape: "gear", coord: { x: 14, y: 12 } },
+      { color: "m", shape: "vortex", coord: { x: 8, y: 10 } },
+    ],
+    tiles = dflt();
+
+  for (const c of rightWallCoord) {
+    tiles[c.x][c.y].right_wall = true;
+  }
+  for (const c of bottomWallCoord) {
+    tiles[c.x][c.y].bottom_wall = true;
+  }
+  for (const g of goals) {
+    tiles[g.coord.x][g.coord.y].goal = g;
+  }
+
+  return { tiles, goals };
+};
+
+function dflt(): Tile[][] {
+  const size = 16;
+  const tiles: Tile[][] = [];
+
+  for (let x = 0; x < size; x++) {
+    const newRow: Tile[] = [];
+    for (let y = 0; y < size; y++) {
+      newRow.push({
+        right_wall: false,
+        bottom_wall: false,
+        robot: null,
+        goal: null,
+        coord: {
+          x: x,
+          y: y,
+        },
+      });
+    }
+
+    tiles.push(newRow);
+  }
+
+  tiles[6][7].right_wall = true;
+  tiles[6][8].right_wall = true;
+  tiles[7][6].bottom_wall = true;
+  tiles[7][8].bottom_wall = true;
+  tiles[8][6].bottom_wall = true;
+  tiles[8][8].bottom_wall = true;
+  tiles[8][7].right_wall = true;
+  tiles[8][8].right_wall = true;
+
+  return tiles;
+}
+
+export const setup: (() => BoardSetup)[] = [b1];
