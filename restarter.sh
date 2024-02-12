ecbbb518f3947044c597616e8ca9281e5e239cf3
whirlwindaster <whirlwindaster@gmail.com>
2024-02-11 20:09:46 -0500

    update restarter

--- restarter.sh
+++ restarter.sh
@@ -1,5 +1,5 @@
 #!
 trap 'kill $(jobs -p)' EXIT; until deno task start & wait; do
-    echo "ldap proxy crashed with exit code $?. Respawning.." >&2
-    sleep 1
+    echo "server crashed with exit code $?. restarting..." >&2
+    sleep 5
 done
